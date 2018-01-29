cc195687e74021249c5932403f35ae448bf71144
tchar989 <tkc218@lehigh.edu>
2018-01-28 22:24:28 -0500

    package/plugin/dependency structure implemented

---
+++ src/main/java/com/spotifymemories/Authorization.java
@@ -0,0 +1,22 @@
+package com.spotifymemories;
+
+/**
+ * Utility class to authorize Spotify user.
+ * @author Tim Chartier
+ */
+public final class Authorization {
+
+    /**
+     * Utility classes should never be instantiated.
+     */
+    private Authorization() {
+
+    }
+
+    /**
+     * Authorize user.
+     */
+    public static void authorize() {
+
+    }
+}
---
+++ src/main/java/com/spotifymemories/Driver.java
@@ -0,0 +1,24 @@
+package com.spotifymemories;
+
+/**
+ * Runs the package.
+ */
+public final class Driver {
+
+    /**
+     * Utility class.
+     */
+    private Driver() {
+
+    }
+
+    /**
+     * Run package.
+     * @param args command-line arguments
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
+
